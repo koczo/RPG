a412d2344a7e737d21cccc9e37fd455564009df6
KOTRAK\michal.koczocik <michal.koczocik@kotrak.com>
2017-09-13 14:45:17 +0200

    PlayerStats

--- RPGProject/Assets/Scripts/Stats/Stat.cs
+++ RPGProject/Assets/Scripts/Stats/Stat.cs
@@ -8,8 +8,25 @@ public class Stat
     [SerializeField]
     private int baseValue;
 
+    private List<int> modifires = new List<int>();
+
     public int GetValue()
     {
-        return baseValue;
+        int finalValue = baseValue;
+        modifires.ForEach(x => finalValue += x);
+
+        return finalValue;
+    }
+
+    public void AddModifire(int modifire)
+    {
+        if (modifire != 0)
+            modifires.Add(modifire);
+    }
+
+    public void RemoveModifire(int modifire)
+    {
+        if (modifire != 0)
+            modifires.Remove(modifire);
     }
 }
