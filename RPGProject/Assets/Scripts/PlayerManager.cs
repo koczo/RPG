e54c52f24f8209f2f504d2dec641232b781d4367
KOTRAK\michal.koczocik <michal.koczocik@kotrak.com>
2017-09-14 11:15:59 +0200

    Create Enemy

---
+++ RPGProject/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager: MonoBehaviour {
+
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
+
+    public GameObject playerObject;
+}
